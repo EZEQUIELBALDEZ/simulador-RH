bf915148bf844d6c5e6ef2063dd12d8ff5f76658
EZEQUIELBALDEZ <128191021+EZEQUIELBALDEZ@users.noreply.github.com>
2023-05-24 21:55:24 -0300

    Add files via upload

---
+++ ezequiel-workspace/LP-SistemaRh-main/LP-SistemaRh-main/iRh/iRh.Windows/Simuladores/frmBeneficioAdicionalNoturno.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace iRh.Windows.Simuladores
+{
+    partial class frmBeneficioAdicionalNoturno
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelResultado = new System.Windows.Forms.Panel();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.lblSalario = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblHorasNoturnas = new System.Windows.Forms.Label();
+            this.panelResultado.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelResultado
+            // 
+            this.panelResultado.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panelResultado.Controls.Add(this.lblResultado);
+            this.panelResultado.Location = new System.Drawing.Point(104, 325);
+            this.panelResultado.Name = "panelResultado";
+            this.panelResultado.Size = new System.Drawing.Size(454, 113);
+            this.panelResultado.TabIndex = 4;
+            this.panelResultado.Visible = false;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(166, 44);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(104, 30);
+            this.lblResultado.TabIndex = 4;
+            this.lblResultado.Text = "Resultado";
+            this.lblResultado.Visible = false;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(241, 280);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(133, 30);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Calcular INSS";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSalario.Location = new System.Drawing.Point(12, 62);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(267, 35);
+            this.txtSalario.TabIndex = 6;
+            // 
+            // lblSalario
+            // 
+            this.lblSalario.AutoSize = true;
+            this.lblSalario.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalario.Location = new System.Drawing.Point(12, 24);
+            this.lblSalario.Name = "lblSalario";
+            this.lblSalario.Size = new System.Drawing.Size(188, 21);
+            this.lblSalario.TabIndex = 5;
+            this.lblSalario.Text = "Informe seu salário bruto:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 156);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(267, 35);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(284, 21);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Quantas horas você trabalhou no mês? ";
+            // 
+            // lblHorasNoturnas
+            // 
+            this.lblHorasNoturnas.AutoSize = true;
+            this.lblHorasNoturnas.Location = new System.Drawing.Point(16, 214);
+            this.lblHorasNoturnas.Name = "lblHorasNoturnas";
+            this.lblHorasNoturnas.Size = new System.Drawing.Size(200, 13);
+            this.lblHorasNoturnas.TabIndex = 10;
+            this.lblHorasNoturnas.Text = "Quantas horas noturnas você trabalhou?";
+            // 
+            // frmBeneficioAdicionalNoturno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblHorasNoturnas);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.lblSalario);
+            this.Controls.Add(this.panelResultado);
+            this.Name = "frmBeneficioAdicionalNoturno";
+            this.Text = "frmBeneficioAdicionalNoturno";
+            this.Load += new System.EventHandler(this.frmBeneficioAdicionalNoturno_Load);
+            this.panelResultado.ResumeLayout(false);
+            this.panelResultado.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelResultado;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.Label lblSalario;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblHorasNoturnas;
+    }
+}
---
+++ ezequiel-workspace/LP-SistemaRh-main/LP-SistemaRh-main/iRh/iRh.Windows/Simuladores/frmBeneficioValeTransporte.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace iRh.Windows.Simuladores
+{
+    partial class frmBeneficioValeTransporte
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(55, 144);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // frmBeneficioValeTransporte
+            // 
+            this.ClientSize = new System.Drawing.Size(502, 350);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Name = "frmBeneficioValeTransporte";
+            this.Text = "Beneficio Vale Transporte";
+            this.Load += new System.EventHandler(this.frmBeneficioValeTransporte_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblSalario;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.TextBox txtTransportePublico;
+        private System.Windows.Forms.Panel panelResultado;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblTransportePublico;
+        private System.Windows.Forms.Button btnCalcularValeTransporte;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
